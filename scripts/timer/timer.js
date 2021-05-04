f85980d154e2ed95f96385af4d9fe6be40d945ad
bagger3025 <bagger3025@g.skku.edu>
2021-05-04 23:22:00 +0900

    Sound Added & ToDo List
    Timer is done, then Sound starts.
    Also, There is ToDo List in ToDo.md File

--- scripts/timer/timer.js
+++ scripts/timer/timer.js
@@ -2,9 +2,12 @@ const timer = document.querySelector(".timer-time"),
 	timerSettingButton = document.querySelector(".timer-settingButton"),
 	timerStartButton = document.querySelector(".timer-start"),
 	timerResetButton = document.querySelector(".timer-reset"),
-	timerStopButton = document.querySelector(".timer-stop");
+	timerStopButton = document.querySelector(".timer-stop"),
+	soundVolume = document.querySelector(".sound-volume");
+
 let interval;
 
+// Old Function, get String to display clock
 function setClock(hour = 0, minute = 0, second = 0) {
 	if (hour === "0" || hour === "") {
 		timer.textContent = `${minute.length < 2 ? "0" + minute : minute} : ${
@@ -17,13 +20,37 @@ function setClock(hour = 0, minute = 0, second = 0) {
 	}
 }
 
+// Get Number, Display clock
 function setClockNumber(hour, minute, second) {
 	timer.textContent = `${hour < 10 ? "0" + hour : hour} : ${
 		minute < 10 ? "0" + minute : minute
 	} : ${second < 10 ? "0" + second : second}`;
 }
 
-function processSetting() {
+let soundOption = new Audio();
+// Read setting panel and get time, and set the time
+function processSetting(target) {
+	const selectedSound = document.querySelector(".selected-sound"),
+		selectedSoundVolume = document.querySelector(".sound-volume");
+	console.dir(selectedSoundVolume);
+	console.log(selectedSoundVolume.valueAsNumber);
+	console.log(target);
+
+	switch (selectedSound.value) {
+		case "Alarm":
+			soundOption = new Audio("./sound/Alarm.mp3");
+			console.log(soundOption);
+			break;
+		case "Organ":
+			soundOption = new Audio("./sound/Organ.mp3");
+			break;
+		default:
+			soundOption = null;
+			break;
+	}
+
+	soundOption.volume = selectedSoundVolume.valueAsNumber;
+
 	const inputs = document
 		.querySelector(".time-setting")
 		.querySelectorAll("input");
@@ -37,9 +64,22 @@ function processSetting() {
 	timerStart();
 }
 
+// When time's up, make alarm
+function makeAlarm() {
+	soundOption.play();
+
+	const stopAlarmButton = document.querySelector(".stop-alarm");
+	stopAlarmButton.addEventListener("click", () => {
+		soundOption.pause();
+		soundOption.currentTime = 0;
+	});
+}
+
+// Make timer run
 function timerCountDown(times) {
-	console.log("Running");
 	if (times.totalsecond === 0) {
+		console.log("Time's Up!");
+		makeAlarm();
 		clearInterval(interval);
 	}
 	times.totalsecond -= times.totalsecond > 0 ? 1 : 0;
@@ -50,6 +90,7 @@ function timerCountDown(times) {
 	);
 }
 
+// Time Trigger Function
 function timerStart() {
 	let currentTime = timer.textContent;
 	let splittedTime = currentTime.split(":");
@@ -86,20 +127,28 @@ function timerStart() {
 	interval = setInterval(timerCountDown, 1000, times);
 }
 
+// Timer Stop Function
 function timerStop() {
 	clearInterval(interval);
 }
 
+// Timer Reset Function
 function timerReset() {
 	clearInterval(interval);
 	setClockNumber(0, 0, 0);
 }
 
+function testInput(e) {
+	console.log(e.target.value, parseFloat(e.target.value) * 100);
+}
+
+// First Function
 function init() {
 	timerSettingButton.addEventListener("click", processSetting);
 	timerStartButton.addEventListener("click", timerStart);
 	timerStopButton.addEventListener("click", timerStop);
 	timerResetButton.addEventListener("click", timerReset);
+	soundVolume.addEventListener("input", testInput);
 }
 
 init();
