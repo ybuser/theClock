8bed0b1203413421be4ead7decca89af01705289
ybuser <ybuser@naver.com>
2021-05-23 18:27:55 +0900

    some addition and changes in side menus

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 const timerButton = document.querySelector(".go-to-timer");
-
 timerButton.addEventListener("click", () => {
 	location.href = "./scripts/timer/timer.html";
 });
 
 const stopwatchButton = document.querySelector(".go-to-stopwatch");
-
 stopwatchButton.addEventListener("click", () => {
 	location.href = "./scripts/stopwatch/stopwatch.html";
 });
+
+const backgroundclockButton = document.querySelector(".go-to-backgroundclock");
+backgroundclockButton.addEventListener("click", () => {
+	location.href = "./scripts/backgroundclock/backgroundclock.html";
+});
+
+const alarmButton = document.querySelector(".go-to-alarm");
+alarmButton.addEventListener("click", () => {
+	location.href = "./scripts/alarm/alarm.html";
+});
+
+const globaltimeButton = document.querySelector(".go-to-globaltime");
+globaltimeButton.addEventListener("click", () => {
+	location.href = "./scripts/globaltime/globaltime.html";
+});
+
